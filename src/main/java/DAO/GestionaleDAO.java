4d449e01461f66ac84ef3b402f69fd7eaa5872cb
sorcho <mariobrosscot@yahoo.it>
2024-01-10 21:42:49 +0100

    added dictionary in GestionaleDAO and corrected Home

--- src/main/java/DAO/GestionaleDAO.java
+++ src/main/java/DAO/GestionaleDAO.java
@@ -4,6 +4,19 @@ import java.sql.Date;
 import java.util.ArrayList;
 
 public interface GestionaleDAO {
+    /**
+     * Ottiene i dati di tutti gli Impiegati dal Database e li inserisce nelle ArrayList apposite
+     * @param listaCF Lista dei Codici Fiscali
+     * @param listaNomi Lista dei Nomi
+     * @param listaCognomi Lista dei Cognomi
+     * @param listaDateNascita Lista delle Date di Nascita
+     * @param listaDateAssunzione Lista delle Date di Assunzione
+     * @param listaEta Lista dei Età
+     * @param listaCategorie Lista delle Categorie ('junior', 'middle', 'senior', 'dirigente')
+     * @param listaSalari Lista dei Salari
+     * @param listaDateScadenza Lista delle Date di Scadenza del contratto a Progetto
+     * @param listaTipiContratto Lista dei Tipi di Contratto ('indeterminato', 'progetto')
+     */
     void getImpiegati(
             ArrayList<String> listaCF,
             ArrayList<String> listaNomi,
@@ -17,6 +30,13 @@ public interface GestionaleDAO {
             ArrayList<String> listaTipiContratto
     );
 
+    /**
+     * Ottiene i dati di tutti i Laboratori dal Database e li inserisce nelle ArrayList apposite
+     * @param listaNomi Lista dei Nomi
+     * @param listaRespSci Lista dei Responsabili Scientifici (la categoria dell'impiegato deve essere 'senior')
+     * @param listaTopic Lista dei Topic
+     * @param listaNAfferenti Lista della quantità di afferenti per singolo Laboratorio
+     */
     void getLaboratori(
             ArrayList<String> listaNomi,
             ArrayList<String> listaRespSci,
@@ -24,6 +44,14 @@ public interface GestionaleDAO {
             ArrayList<Integer> listaNAfferenti
     );
 
+    /**
+     * Ottiene i dati di tutte le Attrezzature dal Database e li inserisce nelle ArrayList apposite
+     * @param listaSeriali Lista dei Seriali
+     * @param listaTipi Lista dei Tipi (macrocategoria della singola Attrezzatura: 'computer', 'tablet', etc.)
+     * @param listaCosti Lista dei Costi
+     * @param listaNomiLab Lista dei Laboratori che possiedono l'Attrezzatura
+     * @param listaCup Lista dei Progetti che acquistano l'Attrezzatura
+     */
     void getAttrezzatura(
             ArrayList<String> listaSeriali,
             ArrayList<String> listaTipi,
@@ -32,6 +60,14 @@ public interface GestionaleDAO {
             ArrayList<String> listaCup
     );
 
+    /**
+     * Ottiene i dati di tutti i Progetti dal Database e li inserisce nelle ArrayList apposite
+     * @param listaCup Lista dei CUP (Codice Unico Progetto)
+     * @param listaRefSci Lista dei Referenti Scientifici (la categoria dell'impiegato deve essere 'senior')
+     * @param listaResp Lista dei Responsabili (la categoria dell'impiegato deve essere 'dirigente')
+     * @param listaNomi Lista dei Nomi
+     * @param listaBudget Lista dei Budget
+     */
     void getProgetti(
             ArrayList<String> listaCup,
             ArrayList<String> listaRefSci,
